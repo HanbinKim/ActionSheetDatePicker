912def7432f9f4619987b2b3bb2f0d9ba42c0f37
HanbinKim <hottuc@naver.com>
2014-01-06 13:22:09 +0900

    Initial Commit

---
+++ ActionSheetDatePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ActionSheetDatePicker
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
